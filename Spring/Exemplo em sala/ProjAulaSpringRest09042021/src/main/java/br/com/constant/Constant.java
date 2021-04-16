69f65239310807bff28fe63718f8f8e471d4a4bf
Leonardo <leonardo_ignacio2010@hotmail.com>
2021-04-16 01:33:18 -0300

    Projeto medicamentos concluído

---
+++ Spring/Exemplo em sala/ProjAulaSpringRest09042021/src/main/java/br/com/constant/Constant.java
@@ -0,0 +1,6 @@
+package br.com.constant;
+
+public class Constant {
+    public static final String API_URL = "projaulaspringrest/api/v1/";
+    public static final String API_CLIENT =  API_URL + "client";
+}
--- Spring/src/main/java/br/com/constant/Constant.java
+++
@@ -1,6 +0,0 @@
-package br.com.constant;
-
-public class Constant {
-    public static final String API_URL = "projspringrest/api/v1";
-    public static final String API_CLIENT = API_URL + "client";
-}
