3d6d34c7f4c8a5dcb3787250cfec79fd92a1d9cb
Leonardo <leonardo_ignacio2010@hotmail.com>
2021-05-28 21:15:37 -0300

    projeto com reddis

---
+++ ProjAvaliacaoSpringCacheReddisDocker/src/main/java/leo/repository/AlunoRepository.java
@@ -0,0 +1,9 @@
+package leo.repository;
+
+import leo.model.Aluno;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlunoRepository extends MongoRepository<Aluno, String> {
+}
